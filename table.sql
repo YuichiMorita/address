fe2aff69a7bc4cd03be5895bf23b0d38b967d272
守田裕一 <morita@s-o-s.jp>
2019-01-04 10:01:48 +0900

    table.sqlを修正
    NOT NULL制約はreferenceよりも前に書かないとエラーになるみたい

--- table.sql
+++ table.sql
@@ -1,3 +1,9 @@
+-- データベース作成
+-- CREATE DATABASE address ENCODING 'UTF-8';
+
+-- このファイルを実行し、テーブルを作成
+-- psql -U postgres -d address -f table.sql
+
 CREATE TABLE user_types( --ユーザー種別
 	id		serial		NOT NULL PRIMARY KEY,
 	type		VARCHAR(100)	NOT NULL, --ユーザー種別[個人、法人]
@@ -6,7 +12,7 @@ CREATE TABLE user_types( --ユーザー種別
 
 CREATE TABLE users(	--ユーザーテーブル
 	id		uuid		NOT NULL PRIMARY KEY,
-	user_types_id	integer		NOT NULL references usertypes(id) ON DELETE RESTRICT, --ユーザー種別[個人、法人]
+	user_types_id	integer		NOT NULL references user_types(id) ON DELETE RESTRICT, --ユーザー種別[個人、法人]
 	company 	VARCHAR(255),	--社名
 	title 		VARCHAR(255),	--肩書き
 	name 		VARCHAR(255)	NOT NULL, --名前
@@ -22,8 +28,8 @@ CREATE TABLE tell_types(	--電話種別テーブル
 
 CREATE TABLE tells(	--電話番号テーブル
 	id		serial		NOT NULL PRIMARY KEY,
-	users_id 	uuid		references users(id) NOT NULL ON DELETE CASCADE, --ユーザーID
-	tell_stypes_id 	integer		references telltypes(id) NOT NULL ON DELETE RESTRICT, --電話番号種別
+	users_id 	uuid	NOT NULL references users(id) ON DELETE CASCADE, --ユーザーID
+	tell_stypes_id 	integer NOT NULL references telltypes(id) ON DELETE RESTRICT, --電話番号種別
 	number		VARCHAR(20)	NOT NULL, --電話番号
 	memo 		TEXT,		--メモ
 	created_at 	TIMESTAMPTZ	NOT NULL DEFAULT NOW()  --作成日時
@@ -38,8 +44,8 @@ CREATE TABLE email_types(	--メール種別テーブル
 
 CREATE TABLE emails(	--メールテーブル
 	id		serial		NOT NULL PRIMARY KEY,
-	users_id 	uuid		references users(id) NOT NULL ON DELETE CASCADE, --ユーザーID
-	email_types_id 	integer		references emailtypes(id) NOT NULL ON DELETE RESTRICT, --メール種別
+	users_id 	uuid NOT NULL references users(id) ON DELETE CASCADE, --ユーザーID
+	email_types_id 	integer NOT NULL	references emailtypes(id) ON DELETE RESTRICT, --メール種別
 	email		VARCHAR(255)	NOT NULL, --メールアドレス
 	memo 		TEXT,		--メモ
 	created_at 	TIMESTAMPTZ	NOT NULL DEFAULT NOW()  --作成日時
@@ -54,15 +60,16 @@ CREATE TABLE addr_types(	--住所種別テーブル
 
 CREATE TABLE addrs(	--住所テーブル
 	id		serial		NOT NULL PRIMARY KEY,
-	users_id 	uuid		references users(id) NOT NULL ON DELETE CASCADE, --ユーザーID
-	addr_types_id 	integer		references addr_types(id) NOT NULL ON DELETE RESTRICT --住所種別
+	users_id 	uuid	NOT NULL 	references users(id) ON DELETE CASCADE, --ユーザーID
+	addr_types_id 	integer	 NOT NULL references addr_types(id) ON DELETE RESTRICT, --住所種別
 	postal 		VARCHAR(7),	--郵便番号
 	pref 		VARCHAR(10),	--都道府県
 	addr1 		VARCHAR(255),	--住所１
 	addr2 		VARCHAR(255),	--住所２
 	bld 		VARCHAR(255),	--建物
-	place_name 	VARCHAR(255)	--場所の識別名[支社名、支店名、サロン名、工場名など]
+	place_name 	VARCHAR(255),	--場所の識別名[支社名、支店名、サロン名、工場名など]
 	memo 		TEXT,		--メモ
+	created_at 	TIMESTAMPTZ	NOT NULL DEFAULT NOW()  --作成日時
 );
 
 CREATE TABLE url_types(	--url種別テーブル
@@ -73,10 +80,11 @@ CREATE TABLE url_types(	--url種別テーブル
 
 CREATE TABLE urls( -- urlテーブル
 	id		serial		NOT NULL PRIMARY KEY,
-	users_id 	uuid		references users(id) NOT NULL ON DELETE CASCADE, --ユーザーID
-	url_types_id 	integer		references url_types(id) NOT NULL ON DELETE RESTRICT, --url種別
+	users_id 	uuid	 NOT NULL 	references users(id) ON DELETE CASCADE, --ユーザーID
+	url_types_id 	integer	NOT NULL 	references url_types(id) ON DELETE RESTRICT, --url種別
 	url 		TEXT, 		--url
 	memo 		TEXT,		--メモ
+	created_at 	TIMESTAMPTZ	NOT NULL DEFAULT NOW()  --作成日時
 );
 
 CREATE TABLE tags( --tagテーブル
@@ -87,6 +95,7 @@ CREATE TABLE tags( --tagテーブル
 
 CREATE TABLE users_tags(	--ユーザー タグの中間テーブル
 	id		serial		NOT NULL PRIMARY KEY,
-	tags_id		integer		references tags(id) NOT NULL ON DELETE CASCADE, --タグID
-	users_id 	uuid		references users(id) NOT NULL ON DELETE CASCADE, --ユーザーID
+	tags_id		integer	NOT NULL references tags(id) ON DELETE CASCADE, --タグID
+	users_id 	uuid	NOT NULL references users(id) ON DELETE CASCADE, --ユーザーID
+	created_at 	TIMESTAMPTZ	NOT NULL DEFAULT NOW()  --作成日時
 );
