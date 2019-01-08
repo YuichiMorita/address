40327085deb0bcb80758d52ec11683f209119e6a
守田裕一 <morita@s-o-s.jp>
2019-01-08 14:34:15 +0900

    エンドポイント調整中
    エンドポイント調整中

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,29 +12,40 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 
+type Uuid = String
+
 data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
+  { id        :: Uuid
+  , company :: Maybe String
+  , title  :: Maybe String
+  , name  :: String
+  , memo  :: Maybe String
   } deriving (Eq, Show)
 
 $(deriveJSON defaultOptions ''User)
 
-type Uuid = String
+users :: [User]
+users = [ User "11111-1111-1111" Nothing Nothing "山田 太郎" Nothing
+        , User "22222-2222-2222" (Just "株式会社井上") (Just "代表取締役") "井上 太郎" (Just "メモメモ")
+        ]
+
+yamada :: String -> User
+yamada _ = users !! 2
 
 type API = "users" :> Get '[JSON] [User]
         :<|> "users" :> Capture "id" Uuid :> Get '[JSON] User
-        :<|> "users" :> ReqBody '[JSON] User :> Post '[JSON] User
-        :<|> "users" :> Capture "id" Uuid :> ReqBody '[JSON] User :> Put '[JSON] User
-        :<|> "users" :> Capture "id" Uuid :> DeleteNoContent '[JSON] NoContent
-        :<|> "tags" :> Get '[JSON] [Tag]
-        :<|> "tags" :> Capture "id" Integer :> Get '[JSON] Tag
-        :<|> "tags" :> ReqBody '[JSON] Tag :> Post '[JSON] Tag
-        :<|> "tags" :> Capture "id" Integer :> ReqBody '[JSON] Tag :> Put '[JSON] Tag
-        :<|> "tags" :> Capture "id" Integer :> DeleteNoContent '[JSON] NoContent
+--      :<|> "users" :> ReqBody '[JSON] User :> Post '[JSON] User
+--      :<|> "users" :> Capture "id" Uuid :> ReqBody '[JSON] User :> Put '[JSON] User
+--      :<|> "users" :> Capture "id" Uuid :> DeleteNoContent '[JSON] NoContent
+--      :<|> "tags" :> Get '[JSON] [Tag]
+--      :<|> "tags" :> Capture "id" Integer :> Get '[JSON] Tag
+--      :<|> "tags" :> ReqBody '[JSON] Tag :> Post '[JSON] Tag
+--      :<|> "tags" :> Capture "id" Integer :> ReqBody '[JSON] Tag :> Put '[JSON] Tag
+--      :<|> "tags" :> Capture "id" Integer :> DeleteNoContent '[JSON] NoContent
 
-startApp :: IO ()
-startApp = run 8080 app
+server :: Server API
+server = return users
+        :<|> return yamada "test"
 
 app :: Application
 app = serve api server
@@ -42,10 +53,5 @@ app = serve api server
 api :: Proxy API
 api = Proxy
 
-server :: Server API
-server = return users
-
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+startApp :: IO ()
+startApp = run 8080 app
