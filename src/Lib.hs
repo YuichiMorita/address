d6d2b04e75bbb9dbe6fef934a1ff450ef1ee8de0
守田裕一 <morita@s-o-s.jp>
2019-01-15 12:00:30 +0900

    データベース設計に応じた肩を一旦全て作成してみた
    [データベース設計に応じた肩を一旦全て作成してみた

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds       #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeOperators   #-}
+
 module Lib
     ( startApp
     , app
@@ -12,14 +13,84 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 
+------------------
+-- 型定義
+------------------
+--UUID
 type Uuid = String
 
+-- ユーザー種別型
+data UserTypes = Indivisual | Company deriving (Eq,Show)
+$(deriveJSON defaultOptions ''UserTypes)
+
+
+-- 電話種別型
+type TellTypes = String
+
+-- 電話番号型
+data Tell = Tell
+    {telltype :: TellTypes
+    ,number :: String
+    ,tellmemo :: Maybe String
+    } deriving(Eq,Show)
+$(deriveJSON defaultOptions ''Tell)
+
+-- メール種別型
+type EmailTypes = String
+
+-- メール型
+data Email = Email
+    {emailtype::EmailTypes
+    ,email :: String
+    ,emailmemo :: Maybe String
+    } deriving(Eq,Show)
+$(deriveJSON defaultOptions ''Email)
+
+-- 住所種別型
+type AddrTypes = String
+
+-- 住所型
+data Address = Address
+    {addrtype :: AddrTypes
+    ,postal :: String
+    ,pref :: String
+    ,addr1 :: String
+    ,addr2 ::String
+    ,bld :: Maybe String
+    ,place_name :: Maybe String
+    ,addrmemo :: Maybe String
+    } deriving(Eq,Show)
+
+$(deriveJSON defaultOptions ''Address)
+
+-- URL種別型
+type UrlTypes = String
+
+-- URL型
+data URL = URL
+    {urltypes :: UrlTypes
+    ,url :: String
+    ,urlmemo :: Maybe String
+        
+    } deriving(Eq,Show)
+$(deriveJSON defaultOptions ''URL)
+
+-- tag型
+type Tag = String
+
+-- ユーザー型
 data User = User
   { id        :: Uuid
+  , usertype :: UserTypes
   , company :: Maybe String
   , title  :: Maybe String
   , name  :: String
   , memo  :: Maybe String
+  , tells :: [Tell]
+  , emails :: [Email]
+  , addrs :: [Address]
+  , urls :: [URL]
+  , tags :: [Tag]
   } deriving (Eq, Show)
 
 $(deriveJSON defaultOptions ''User)
@@ -42,8 +113,8 @@ type API = "users" :> Get '[JSON] [User]
 -- ハンドラ
 ------------------
 users :: [User]
-users =  [ User "11111-1111-1111" Nothing Nothing "山田 太郎" Nothing
-        , User "22222-2222-2222" (Just "株式会社井上") (Just "代表取締役") "井上 太郎" (Just "メモメモ")
+users =  [ User "11111-1111-1111" Indivisual Nothing Nothing "山田 太郎" Nothing [] [] [] [] ["個人","Reju"]
+        , User "22222-2222-2222" Company (Just "株式会社井上") (Just "代表取締役") "井上 太郎" (Just "メモメモ") [Tell "携帯電話" "090-1111-1111" Nothing] [Email "勤務先" "info@iinoue.com" Nothing] [Address "勤務先" "530-0053" "大阪府" "大阪市北区末広町" "3-13" (Just "扇町松島ビル9F") Nothing Nothing] [URL "勤務先" "http://www.reju.jp/" Nothing] ["法人","Reju","REX"]
         ]
 -- /users
 allUsers :: Handler [User]
